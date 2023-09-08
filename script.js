f03fed67668009340c82046fe9254e0634267afb
Shivam Jadhav <127479563+shivam7695@users.noreply.github.com>
2023-09-08 23:09:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-	touristSpots.sort();
+	touristSpots.sort((a,b)=>{
+		if(a>b){
+return 1
+		}else if(a<b){
+			return -1;
+			
+		}
+		return 0;
+	});
 console.log(touristSpots);
